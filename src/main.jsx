ec0d3aca753de43e70443f14c258663c26bd954d
arandombit <a.random.luis@proton.me>
2026-01-24 22:52:45 -0500

    wrap app with query client provider to enable react query

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import './index.css'
 
 import App from './App.jsx'
 
-const queryClient = new QueryClient()
+const client = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider {...{ client }}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 )

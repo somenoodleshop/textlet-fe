0f367aed75cd78e6c119b48af0bca06d1370758c
arandombit <a.random.luis@proton.me>
2026-01-25 23:36:58 -0500

    integrate fetch to query hook to retrieve comments

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const { VITE_API_URL: API_URL = '' } = import.meta.env
 const get = url => fetch(url).then(response => !response.ok ? Promise.reject(response) : response.json())
 
 const App = () => {
-  const query = useQuery({ queryKey: 'comments', queryFn: () => {} })
+  const query = useQuery({ queryKey: 'comments', queryFn: () => get(`${API_URL}/comments`) })
   return (
     <div>
       <h1 className='text-3xl font-bold underline'>Hello World</h1>

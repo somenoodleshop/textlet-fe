5c9ca8c2a2e768316bb9fd41613dc2b1ce3257c9
arandombit <a.random.luis@proton.me>
2026-01-25 23:29:15 -0500

    initialize query hook for getting comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useQuery } from '@tanstack/react-query'
 import './App.css'
 
 const App = () => {
+  const query = useQuery({ queryKey: 'comments', queryFn: () => {} })
   return (
     <div>
       <h1 className='text-3xl font-bold underline'>Hello World</h1>

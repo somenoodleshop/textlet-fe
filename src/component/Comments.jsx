b9c46012d6befe1ad410f02bc7ddd39a90a8f183
arandombit <a.random.luis@proton.me>
2026-01-28 23:32:00 -0500

    remove loading state ui from comments component

--- src/component/Comments.jsx
+++ src/component/Comments.jsx
@@ -1,11 +1,5 @@
 const Comments = props => (
   <>
-    { props.isLoading && (
-      <div className='flex items-center gap-2 text-gray-600'>
-        <div className='w-5 h-5 border-2 border-gray-300 border-t-gray-600 rounded-full animate-spin' />
-        <p className='text-lg'>Loading...</p>
-      </div>
-    ) }
     { props.isError && (
       <div className='p-4 bg-red-50 border border-red-200 rounded-lg'>
         <p className='text-red-800 font-semibold'>Error</p>

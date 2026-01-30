81f40afd7d964a1569d5acb47c6e7c1ff200d560
arandombit <a.random.luis@proton.me>
2026-01-29 22:33:15 -0500

    remove gray bg from app

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
   const mutation = useMutation({ mutationFn: comment => post(`${API_URL}/comments`, comment) })
   const [comment, setComment] = useState('')
   return (
-    <div className='min-h-screen p-8 bg-gray-50'>
+    <div className='min-h-screen p-8'>
       <h1 className='text-3xl font-bold underline mb-6'>Hello World</h1>
       <Comments {...query} />
       <form onSubmit={handleSubmit({ comment, mutation, setComment })}>

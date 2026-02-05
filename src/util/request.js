f9d8a9ce96c82607a455d62b54cd20db93bf05db
arandombit <a.random.luis@proton.me>
2026-02-05 23:00:00 +0100

    add post function to request util

--- src/util/request.js
+++ src/util/request.js
@@ -2,3 +2,9 @@ const onSuccess = response =>
   !response.ok ? Promise.reject(response) : response.json()
 
 const get = url => fetch(url).then(onSuccess)
+const post = url => data =>
+  fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  }).then(onSuccess)

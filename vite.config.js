c84d8c73b61e8e7cff60f16ff508a06b8e5b5575
arandombit <a.random.luis@proton.me>
2026-02-10 23:00:00 +0100

    import tanstack router plugin into vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
